647d85a7f874b4ec18c0484066d9d275fb2f3cf1
NFajardo98 <65809295+NFajardo98@users.noreply.github.com>
2024-10-21 12:57:34 +0200

    Practica 4
    Ejercicio 1 completo hecho por Nicolás.
    Para ver las bases de datos creadas descargar extension sqlite en vs code

--- Práctica4/pr4.py
+++ Práctica4/pr4.py
@@ -3,8 +3,11 @@ TODO: rellenar
 
 Asignatura: GIW
 Práctica 4
-Grupo: XXXXXXX
-Autores: XXXXXX 
+Grupo: 07
+Autores:    NICOLÁS JUAN FAJARDO CARRASCO
+            PABLO GARCÍA FERNÁNDEZ
+            MANUEL LOURO MENESES
+            ROBERTO MORENO GUILLÉN
 
 Declaramos que esta solución es fruto exclusivamente de nuestro trabajo personal. No hemos
 sido ayudados por ninguna otra persona o sistema automático ni hemos obtenido la solución
@@ -15,12 +18,68 @@ resultados de los demás.
 """
 
 
-def crear_bd(db_filename):
-    ...
+import sqlite3
+import csv
+from datetime import datetime
 
+def crear_bd(db_name):
+    # Conectamos o creamos la bd
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
 
-def cargar_bd(db_filename, tab_datos, tab_ibex35):
-    ...
+    # Crea la tabla datos_generales
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS datos_generales (
+            ticker TEXT PRIMARY KEY,
+            nombre TEXT,
+            indice TEXT,
+            pais TEXT
+        )
+    ''')
+
+    # Crea la tabla semanales_IBEX35
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS semanales_IBEX35 (
+            ticker TEXT,
+            fecha TEXT,
+            precio REAL,
+            PRIMARY KEY (ticker, fecha),
+            FOREIGN KEY (ticker) REFERENCES datos_generales(ticker)
+        )
+    ''')
+
+    # Guarda los cambios y cerramos la conexión
+    conn.commit()
+    conn.close()
+
+def cargar_bd(db_name, tab_datos, tab_ibex35):
+    # Conectamios a la bd
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
+
+    # Carga desde los CSV
+    with open(tab_datos, 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter=';')  
+        for row in reader:
+            cur.execute('''
+                INSERT OR IGNORE INTO datos_generales (ticker, nombre, indice, pais)
+                VALUES (?, ?, ?, ?)
+            ''', (row['ticker'], row['nombre'], row['indice'], row['pais']))
+
+    with open(tab_ibex35, 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter=';')  
+        for row in reader:
+            # Convertir la fecha al formato correcto (ISO-8601)
+            fecha_procesada = datetime.strptime(row['fecha'], '%d/%m/%Y %H:%M').strftime('%Y-%m-%d %H:%M')
+            cur.execute('''
+                INSERT OR IGNORE INTO semanales_IBEX35 (ticker, fecha, precio)
+                VALUES (?, ?, ?)
+            ''', (row['ticker'], fecha_procesada, row['precio']))
+
+    # Guardamos los datos y cerramos la conexión
+    conn.commit()
+    conn.close()
+
 
 
 def consulta1(db_filename, indice):
@@ -37,3 +96,4 @@ def consulta3(db_filename, limite):
 
 def consulta4(db_filename, ticker):
     ...
+
