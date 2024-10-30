e8163c271eaea485cdaa9ebbf5c3be94153e6d67
Pablo <pagarc24@ucm.es>
2024-10-30 17:33:38 +0100

    Estructura práctica 8

---
+++ Práctica8/pr8.py
@@ -0,0 +1,20 @@
+"""
+TODO: rellenar
+
+Asignatura: GIW
+Práctica 8
+Grupo: XXXXXXX
+Autores: XXXXXX 
+
+Declaramos que esta solución es fruto exclusivamente de nuestro trabajo personal. No hemos
+sido ayudados por ninguna otra persona o sistema automático ni hemos obtenido la solución
+de fuentes externas, y tampoco hemos compartido nuestra solución con otras personas
+de manera directa o indirecta. Declaramos además que no hemos realizado de manera
+deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perjudicar los
+resultados de los demás.
+"""
+
+###
+### <DEFINIR AQUÍ LAS CLASES DE MONGOENGINE>
+###
+
