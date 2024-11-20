c81bbbaac931bf1623d99e1e837ec28df0e3ed97
Pablo <pagarc24@ucm.es>
2024-11-20 19:48:17 +0100

    Update pr7.py
    Fin

--- Práctica07/pr7.py
+++ Práctica07/pr7.py
@@ -20,7 +20,6 @@ from jsonschema import validate, ValidationError
 
 app = Flask(__name__)
 
-
 ###
 ### <DEFINIR AQUI EL SERVICIO REST>
 ###
@@ -108,91 +107,118 @@ def get_asignaturas():
         end = start + per_page if start + per_page < len(asignaturas) else len(asignaturas)
 
         asignaturas_urls = [f"/asignaturas/{aid}"
-                            for aid in asignaturas.keys()
-                            if asignaturas[aid]["numero_alumnos"] >= min_alumnos][start:end]
+                            for aid, asig in asignaturas.items()
+                            if asig["numero_alumnos"] >= min_alumnos][start:end]
         code = 200 if len(asignaturas_urls) == len(asignaturas) else 206
 
-        return jsonify({"asignaturas": asignaturas_urls}), code
+        response_content = jsonify({"asignaturas": asignaturas_urls})
     elif per_page is None and page is None:
         asignaturas_urls = [f"/asignaturas/{aid}"
-                            for aid in asignaturas.keys()
-                            if asignaturas[aid]["numero_alumnos"] >= min_alumnos]
+                            for aid, asig in asignaturas.items()
+                            if asig["numero_alumnos"] >= min_alumnos]
         code = 200 if len(asignaturas_urls) == len(asignaturas) else 206
 
-        return jsonify({"asignaturas": asignaturas_urls}), code
+        response_content = jsonify({"asignaturas": asignaturas_urls})
     else:
-        return '', 400
+        response_content = ''
+        code = 400
+    return response_content, code
 
 # GET, DELETE, PUT, PATCH asignatura específica
-@app.route('/asignaturas/<int:id>', methods=['GET', 'DELETE', 'PUT', 'PATCH'])
-def asignatura(id):
-    """Método para obtener, eliminar, modificar o actualizar una asignatura específica"""
+@app.route('/asignaturas/<int:id_asignatura>', methods=['GET', 'DELETE', 'PUT', 'PATCH'])
+def asignatura(id_asignatura):
+    """
+    Metodo que se encarga de localizar la función correspondiente ('GET', 'DELETE', 'PUT', 'PATCH')
+    a la petición para realizar a la asignatura definida por id.
+    """
     if request.method == 'GET':
-        if id in asignaturas:
-            return jsonify(asignaturas[id]), 200
-        else:
-            return '', 404
-
+        response_content, code = get_asignatura_aux(id_asignatura)
     elif request.method == 'DELETE':
-        if id in asignaturas:
-            del asignaturas[id]
-            return '', 204
-        else:
-            return '', 404
-
+        response_content, code = delete_asignatura_aux(id_asignatura)
     elif request.method == 'PUT':
-        data = request.get_json()
-        if id not in asignaturas:
-            return '', 404
-
-        try:
-            validate(instance=data, schema=asignatura_schema)
-        except ValidationError as validation_error:
-            return jsonify({"error": f"Formato incorrecto: {validation_error.message}"}), 400
-
-        asignaturas[id] = {
-            "id": id,
-            "nombre": data["nombre"],
-            "numero_alumnos": data["numero_alumnos"],
-            "horario": data["horario"]
-        }
-        return '', 200
-
+        response_content, code = put_asignatura_aux(id_asignatura)
     elif request.method == 'PATCH':
-        if id not in asignaturas:
-            return '', 404
-
-        data = request.get_json()
-        if len(data.keys()) != 1:
-            return '', 400
-
-        clave = list(data.keys())[0]
-        if clave not in ["nombre", "numero_alumnos", "horario"]:
-            return '', 400
-
-        try:
-            validate(instance={clave: data[clave]},
-                    schema={"type": "object", "properties":
-                    {clave: asignatura_schema["properties"][clave]}, "required": [clave]})
-        except ValidationError as validation_error:
-            return jsonify({"error": f"Formato incorrecto: {validation_error.message}"}), 400
-
-        asignaturas[id][clave] = data[clave]
-        return '', 200
+        response_content, code = patch_asignatura_aux(id_asignatura)
+    return response_content, code
 
 # Ruta para obtener el horario de una asignatura
-@app.route('/asignaturas/<int:id>/horario', methods=['GET'])
-def get_horario_asignatura(id):
+@app.route('/asignaturas/<int:id_asignatura>/horario', methods=['GET'])
+def get_horario_asignatura(id_asignatura):
+    """Método para obtener el horario de una asignatura"""
     # Verificamos si la asignatura existe
-    if id not in asignaturas:
+    if id_asignatura not in asignaturas:
         return '', 404  # Asignatura no encontrada
 
     # Obtenemos y devolvemos el horario de la asignatura
-    horario = asignaturas[id].get("horario", [])
+    horario = asignaturas[id_asignatura].get("horario", [])
     return jsonify({"horario": horario}), 200
 
-### FIN DEL SERVICIO REST ###
 
+### FUNCIONES AUXILIARES
+
+def get_asignatura_aux(id_asignatura):
+    """Método para obtener una asignatura específica"""
+    if id_asignatura in asignaturas:
+        response_content = jsonify(asignaturas[id_asignatura])
+        code = 200
+    else:
+        response_content = ''
+        code = 404
+    return response_content, code
+
+def delete_asignatura_aux(id_asignatura):
+    """Método para eliminar una asignatura específica"""
+    response_content = ''
+    if id_asignatura in asignaturas:
+        del asignaturas[id_asignatura]
+        code = 204
+    else:
+        code = 404
+    return response_content, code
+
+def put_asignatura_aux(id_asignatura):
+    """Método para modificar una asignatura específica"""
+    data = request.get_json()
+    if id_asignatura not in asignaturas:
+        return '', 404
+
+    try:
+        validate(instance=data, schema=asignatura_schema)
+    except ValidationError as validation_error:
+        return jsonify({"error": f"Formato incorrecto: {validation_error.message}"}), 400
+
+    asignaturas[id_asignatura] = {
+        "id": id_asignatura,
+        "nombre": data["nombre"],
+        "numero_alumnos": data["numero_alumnos"],
+        "horario": data["horario"]
+    }
+    return '', 200
+
+def patch_asignatura_aux(id_asignatura):
+    """Método para modificar parcialmente una asignatura específica"""
+    if id_asignatura not in asignaturas:
+        return '', 404
+
+    data = request.get_json()
+    if len(data.keys()) != 1:
+        return '', 400
+
+    clave = list(data.keys())[0]
+    if clave not in ["nombre", "numero_alumnos", "horario"]:
+        return '', 400
+
+    try:
+        validate(instance={clave: data[clave]},
+                schema={"type": "object", "properties":
+                {clave: asignatura_schema["properties"][clave]}, "required": [clave]})
+    except ValidationError as validation_error:
+        return jsonify({"error": f"Formato incorrecto: {validation_error.message}"}), 400
+
+    asignaturas[id_asignatura][clave] = data[clave]
+    return '', 200
+
+### FIN DEL SERVICIO REST ###
 
 if __name__ == '__main__':
     # Activa depurador y recarga automáticamente
