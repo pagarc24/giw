93335a52fdc269d1d0f960fdce1ea39057ffc846
RMG02 <robmor03@ucm.es>
2024-12-11 22:26:19 +0100

    Práctica10

--- Práctica10/pr10.py
+++ Práctica10/pr10.py
@@ -15,7 +15,10 @@ deshonesta ninguna otra actividad que pueda mejorar nuestros resultados ni perju
 resultados de los demás.
 """
 
-from flask import Flask, request, session
+import hashlib
+import os
+from flask import Flask, json, make_response, redirect, request, session
+import requests
 # Resto de importaciones
 
 
@@ -23,26 +26,68 @@ app = Flask(__name__)
 
 
 # Credenciales
-CLIENT_ID = 'XXXXXX'
-CLIENT_SECRET = 'YYYYYY'
+CLIENT_ID = ''
+CLIENT_SECRET = ''
 
 REDIRECT_URI = 'http://localhost:5000/token'
 
-# Fichero de descubrimiento para obtener el 'authorization endpoint' y el 
+# Fichero de descubrimiento para obtener el 'authorization endpoint' y el
 # 'token endpoint'
 DISCOVERY_DOC = 'https://accounts.google.com/.well-known/openid-configuration'
 
 
 @app.route('/login_google', methods=['GET'])
 def login_google():
-    ...
 
+    token_antifalsificación = hashlib.sha256(os.urandom(1024)).hexdigest()
+    session['CSRF'] = token_antifalsificación
+
+    respuesta_json = requests.get(DISCOVERY_DOC).json()
+    authorization_endpoint = respuesta_json["authorization_endpoint"]
+
+    url = (
+        f"{authorization_endpoint}?client_id={CLIENT_ID}&response_type=code"
+        f"&scope=openid%20email&redirect_uri={REDIRECT_URI}&state={token_antifalsificación}"
+    )
+
+    return redirect(url)
 
 @app.route('/token', methods=['GET'])
 def token():
-    ...
+    if request.args.get('state') != session.get('CSRF'):
+        response = make_response(json.dumps('Token invalido'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
 
-        
+    code = request.args.get("code")
+
+    respuesta_json = requests.get(DISCOVERY_DOC).json()
+    token_endpoint = respuesta_json["token_endpoint"]
+
+    datos_token = {
+        'code': code,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'redirect_uri': REDIRECT_URI,
+        'grant_type': 'authorization_code'
+    }
+    respuesta_token = requests.post(token_endpoint, data=datos_token)
+    token_respuesta_json = respuesta_token.json()
+    id_token = token_respuesta_json['id_token']
+
+    url_tokeninfo = f'https://oauth2.googleapis.com/tokeninfo?id_token={id_token}'
+    respuesta_tokeninfo = requests.get(url_tokeninfo)
+
+    if respuesta_tokeninfo.status_code == 200:
+        usuario_info = respuesta_tokeninfo.json()
+        email = usuario_info['email']
+        return f"Bienvenido {email}"
+    else:
+        return "Error con tokeninfo", 400
+
+
+
+
 if __name__ == '__main__':
     # Activa depurador y recarga automáticamente
     app.config['ENV'] = 'development'
